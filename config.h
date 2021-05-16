ff60c6108c0c7bbce331ac7b6ff3862c06f19f98
Ahmed Nabeel <nabeel910@protonmail.com>
2021-05-16 10:03:16 +0200

    highlighting

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "Roboto Mono Nerd Font:size=11","Noto Color Emoji:pixelsize=14:antialias=true:autohint=true" };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = " " ;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ABB2BF", "#191724" },
@@ -14,8 +14,8 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 35;
-static unsigned int min_lineheight = 35;
+static unsigned int lineheight = 30;
+static unsigned int min_lineheight = 30;
 
 /*
  * Characters not considered part of a word while deleting words
