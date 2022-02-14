e9c6956abec490490b00b614ae66b9e74f9d6ea6
Ahmed Nabeel <nabeel910@protonmail.com>
2022-02-14 09:02:24 +0200

    update

---
+++ themes/default.h
@@ -0,0 +1,7 @@
+//default dmenu color scheme
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
