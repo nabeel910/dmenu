90c03cff457fa54a1bd3a850a2276660f8c4f342
Ahmed Nabeel <nabeel910@protonmail.com>
2022-03-14 19:37:08 +0200

    update

--- config.h
+++ config.h
@@ -3,12 +3,13 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+
 static const char *fonts[] = {
 "TerminessTTF Nerd Font:style=bold:size=13","JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 
 static const char *colors[SchemeLast][2] = {
-#include "/home/nabeel/apps/suckless/dmenu/themes/dracula.h"
+#include "/home/nabeel/apps/suckless/dmenu/themes/catppuccin.h"
 };
 static const char *prompt      = " ";      /* -p  option; prompt to the left of input field */
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
---
+++ themes/catppuccin.h
@@ -0,0 +1,5 @@
+//catppuccin
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d9e0ee", "#1e1e2e" },
+	[SchemeSel] = { "#1e1e2e", "#89DCEB" },
+	[SchemeOut] = { "#1e1e2e", "#d9e0ee" },
